715f5ab53d56b5a31ea768e59f2a17e1eebde0e3
Quentin Dreyer <quentin.dreyer@gmail.com>
2020-07-02 11:53:22 +0200

    chore: add redeliveredAt in RedeliveryStamp construct
    this would allows one to correctly unserialise a RedeliveryStamp with a custom serializer not using the php serialize function

--- Stamp/RedeliveryStamp.php
+++ Stamp/RedeliveryStamp.php
@@ -24,12 +24,12 @@ final class RedeliveryStamp implements StampInterface
     private $exceptionMessage;
     private $flattenException;
 
-    public function __construct(int $retryCount, string $exceptionMessage = null, FlattenException $flattenException = null)
+    public function __construct(int $retryCount, string $exceptionMessage = null, FlattenException $flattenException = null, \DateTimeTimmutable $redeliveredAt = new \DateTimeImmutable())
     {
         $this->retryCount = $retryCount;
         $this->exceptionMessage = $exceptionMessage;
         $this->flattenException = $flattenException;
-        $this->redeliveredAt = new \DateTimeImmutable();
+        $this->redeliveredAt = $redeliveredAt;
     }
 
     public static function getRetryCountFromEnvelope(Envelope $envelope): int
